1edd739d3ae235845b8e0c3d87a0e5a111f65a84
azmain-siam <siamhossain2700@gmail.com>
2024-05-13 20:53:03 +0600

    My posted jobs Done

--- src/pages/ManagePost.jsx
+++ src/pages/ManagePost.jsx
@@ -5,8 +5,46 @@ import { LuTrash2 } from "react-icons/lu";
 import { Link } from "react-router-dom";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import useAuth from "../hooks/useAuth";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Bars } from "react-loader-spinner";
 
 const ManagePost = () => {
+  const { user } = useAuth();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const getData = async () => {
+      const { data } = await axios(
+        `${import.meta.env.VITE_URL}/post/${user.email}`
+      );
+      setPosts(data);
+    };
+    getData();
+    setLoading(false);
+  }, [user]);
+
+  console.log(posts);
+
+  if (loading) {
+    return (
+      <div className="flex h-full w-full justify-center items-center pt-20">
+        <Bars
+          height="80"
+          width="80"
+          color="#E9424F"
+          ariaLabel="bars-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="my-5 max-w-7xl w-[95%] md:w-[93%] mx-auto mt-10 md:mt-14">
       {/* <Helmet>
@@ -19,7 +57,7 @@ const ManagePost = () => {
       </div>
       <div className="">
         <Tabs>
-          <div className="font-semibold flex justify-center">
+          <div className="font-semibold">
             <TabList>
               <Tab>Need Volunteer Posts</Tab>
               <Tab>Volunteer Request Post</Tab>
@@ -27,7 +65,7 @@ const ManagePost = () => {
           </div>
 
           <TabPanel>
-            <div className="overflow-x-auto mt-5 border border-[#e4e4e4] dark:border-gray-500 rounded-lg">
+            <div className="overflow-x-auto mt-7 border border-[#e4e4e4] dark:border-gray-500 rounded-lg">
               <table className="table">
                 <thead className="text-sm text-gray-500 dark:text-gray-400">
                   <tr className="border-[#e4e4e4] dark:border-gray-500">
@@ -38,27 +76,32 @@ const ManagePost = () => {
                   </tr>
                 </thead>
                 <tbody className="text-sm">
-                  <tr className="font-semibold border-[#e4e4e4] dark:border-gray-500">
-                    <td>item.itemNam</td>
-                    <td>item.category</td>
-                    <td>item.price</td>
-                    <td className="flex items-center justify-center gap-4">
-                      <Link to={"/posts/update"} className="col-span-1 ">
-                        <FaRegPenToSquare
-                          title="Edit"
-                          className="text-[#30A458] hover:scale-[1.15] duration-300"
-                          size={19}
-                        />
-                      </Link>
-                      <Link className="col-span-1 ">
-                        <LuTrash2
-                          title="Delete"
-                          className="text-[#E7404C] hover:scale-[1.15] duration-300"
-                          size={21}
-                        />
-                      </Link>
-                    </td>
-                  </tr>
+                  {posts.map((post) => (
+                    <tr
+                      key={post._id}
+                      className="font-semibold border-[#e4e4e4] dark:border-gray-500"
+                    >
+                      <td>{post.post_title}</td>
+                      <td>{post.category}</td>
+                      <td>{new Date(post.deadline).toLocaleDateString()}</td>
+                      <td className="flex items-center justify-center gap-4">
+                        <Link to={"/posts/update"} className="col-span-1 ">
+                          <FaRegPenToSquare
+                            title="Edit"
+                            className="text-[#30A458] hover:scale-[1.15] duration-300"
+                            size={19}
+                          />
+                        </Link>
+                        <Link className="col-span-1 ">
+                          <LuTrash2
+                            title="Delete"
+                            className="text-[#E7404C] hover:scale-[1.15] duration-300"
+                            size={21}
+                          />
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
